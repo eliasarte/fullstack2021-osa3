487ec1ef065f2613a7a9b3436a874d57018018ac
Elias <elias.arte@live.com>
2021-05-10 14:36:06 +0300

    osa 3 3.2

--- index.js
+++ index.js
@@ -1,34 +1,41 @@
 const express = require('express')
 const app = express()
 
-let persons = {
-    "persons": [
-      {
+let persons = [
+    {
         "name": "Arto Hellas",
         "number": "040-123456",
         "id": 1
-      },
-      {
+    },
+    {
         "name": "Ada Lovelace",
         "number": "39-44-5323523",
         "id": 2
-      },
-      {
+    },
+    {
         "name": "Dan Abramov",
         "number": "12-43-234345",
         "id": 3
-      },
-      {
+    },
+    {
         "name": "Mary Poppendieck",
         "number": "39-23-6423122",
         "id": 4
-      }
-    ]
-  }
+    }
+]
+
 
 app.get('/api/persons', (req, res) => {
   res.json(persons)
 })
+
+app.get('/info', (req, res) => {
+    const time = new Date();
+    const count = persons.length
+    res.send(
+        `<div><p>Phonebook has info for ${count} people</p><p>${time}</p></div>`
+    )
+  })
 
 const PORT = 3001
 app.listen(PORT, () => {
