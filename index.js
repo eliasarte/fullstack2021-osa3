54a57e49a388f802967744643a11a0ea5ab6611e
eliasarte <elias.arte@live.com>
2021-05-17 19:05:57 +0300

    osa3 3.20

--- index.js
+++ index.js
@@ -11,13 +11,18 @@ app.use(express.json())
 morgan.token('body', (req, res) => JSON.stringify(req.body))
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
-const errorHandler = (error, req, re, next) => {
+const errorHandler = (error, request, response, next) => {
     console.error(error.message)
+  
     if (error.name === 'CastError') {
-      return res.status(400).send({ error: 'malformatted id' })
+        return response.status(400).send({ error: 'malformatted id' })
     }
+    else if (error.name === 'ValidationError') {
+        return response.status(400).json({ error: error.message })
+    }
+  
     next(error)
-  }
+}
   
 const requestLogger = (request, response, next) => {
     console.log('Method:', request.method)
