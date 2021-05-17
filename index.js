f7e427d6a72b367646f273bd9479c7395b7bda98
eliasarte <elias.arte@live.com>
2021-05-17 15:46:00 +0300

    osa3 3.10

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require('express')
 const morgan = require('morgan')
 const app = express()
+const cors = require('cors')
 
+app.use(express.static('build'))
+app.use(cors())
 app.use(express.json())
 morgan.token('body', (req, res) => JSON.stringify(req.body))
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
@@ -89,7 +92,7 @@ app.post("/api/persons", (req, res) => {
     res.json(person);
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
